4c1facbbf7b2d4b607128d600cc51cf5133b97a3
Moza Sajidah <moza.sajidah@gmail.com>
2023-05-21 19:49:01 +0300

    Tugas 2.3 Basic Component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Users from "./components/Users";
+import Products from "./components/Products";
+
+function App() {
+  return (
+    <div>
+      <Users />
+      <Products />
+    </div>
+  );
+}
+
+export default App;
